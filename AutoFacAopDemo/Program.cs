4c643182ea81bdf09f629cc028c154dc7fd75ee1
shaolixin <shaolixin@yiche.com>
2020-06-17 10:27:17 +0800

    Emit Demo

--- AutoFacAopDemo/Program.cs
+++ AutoFacAopDemo/Program.cs
@@ -1,22 +1,101 @@
 ﻿using Autofac;
 using Autofac.Extras.DynamicProxy;
-using Castle.DynamicProxy;
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AutoFacAopDemo
 {
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
     class Program
     {
+        Person p = new Person();
         static void Main(string[] args)
         {
-            Start();
-            //  Console.WriteLine("Hello World!");
+            // Define the cancellation token.
+            CancellationTokenSource source = new CancellationTokenSource();
+            CancellationToken token = source.Token;
+
+            Random rnd = new Random();
+            Object lockObj = new Object();
+
+            List<Task<int[]>> tasks = new List<Task<int[]>>();
+            TaskFactory factory = new TaskFactory(token);
+            for (int taskCtr = 0; taskCtr <= 10; taskCtr++)
+            {
+                int iteration = taskCtr + 1;
+                tasks.Add(factory.StartNew(() =>
+                {
+                    int value;
+                    int[] values = new int[10];
+                    for (int ctr = 1; ctr <= 10; ctr++)
+                    {
+                        lock (lockObj)
+                        {
+                            value = rnd.Next(0, 101);
+                        }
+                        if (value == 0)
+                        {
+                            source.Cancel();
+                            Console.WriteLine("Cancelling at task {0}", iteration);
+                            break;
+                        }
+                        values[ctr - 1] = value;
+                    }
+                    return values;
+                }, token));
+            }
+            try
+            {
+                Task<double> fTask = factory.ContinueWhenAll(tasks.ToArray(),
+                                                             (results) =>
+                                                             {
+                                                                 Console.WriteLine("Calculating overall mean...");
+                                                                 long sum = 0;
+                                                                 int n = 0;
+                                                                 foreach (var t in results)
+                                                                 {
+                                                                     foreach (var r in t.Result)
+                                                                     {
+                                                                         sum += r;
+                                                                         n++;
+                                                                     }
+                                                                 }
+                                                                 return sum / (double)n;
+                                                             }, token);
+                Console.WriteLine("The mean is {0}.", fTask.Result);
+            }
+            catch (AggregateException ae)
+            {
+                foreach (Exception e in ae.InnerExceptions)
+                {
+                    if (e is TaskCanceledException)
+                        Console.WriteLine("Unable to compute mean: {0}",
+                                          ((TaskCanceledException)e).Message);
+                    else
+                        Console.WriteLine("Exception: " + e.GetType().Name);
+                }
+            }
+            finally
+            {
+                source.Dispose();
+            }
+            Console.ReadKey();
         }
+
+        // Start();
+        //  Console.WriteLine("Hello World!");
+
         static void Start()
         {
             var builder = new ContainerBuilder();
-         //   builder.Register(c => new CallLogger(Console.Out)).Named<IInterceptor>("log-calls");
-             builder.Register(c => new CallLogger(Console.Out));
+            //   builder.Register(c => new CallLogger(Console.Out)).Named<IInterceptor>("log-calls");
+            builder.Register(c => new CallLogger(Console.Out));
             //  builder.RegisterType<Circle>().EnableClassInterceptors();
             //  builder.RegisterType<Circle>().EnableInterfaceInterceptors();
             //动态注入拦截器CallLogger
---
+++ EmitDemo/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace EmitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AssemblyName aName = new AssemblyName("DynamicAssemblyExample");
+
+            var ab = AssemblyBuilder.DefineDynamicAssembly(aName, AssemblyBuilderAccess.RunAndCollect);
+            ModuleBuilder mb = ab.DefineDynamicModule(aName.Name + ".dll");
+            TypeBuilder tb = mb.DefineType("MyDynamicType", TypeAttributes.Public);
+            FieldBuilder fbNumber = tb.DefineField("m_number", typeof(int), FieldAttributes.Private);
+            Type[] parameterTypes = { typeof(int) };
+            ConstructorBuilder ctor1 = tb.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard, parameterTypes);
+
+            ILGenerator ctor1IL = ctor1.GetILGenerator();
+            ctor1IL.Emit(OpCodes.Ldarg_0);
+            ctor1IL.Emit(OpCodes.Call, typeof(object).GetConstructor(Type.EmptyTypes));
+
+            ctor1IL.Emit(OpCodes.Ldarg_0);
+            ctor1IL.Emit(OpCodes.Ldarg_1);
+            ctor1IL.Emit(OpCodes.Stfld, fbNumber);
+            ctor1IL.Emit(OpCodes.Ret);
+
+            // Define a default constructor that supplies a default value
+            // for the private field. For parameter types, pass the empty
+            // array of types or pass null.
+            ConstructorBuilder ctor0 = tb.DefineConstructor(
+                MethodAttributes.Public,
+                CallingConventions.Standard,
+                Type.EmptyTypes);
+
+            ILGenerator ctor0IL = ctor0.GetILGenerator();
+            // For a constructor, argument zero is a reference to the new
+            // instance. Push it on the stack before pushing the default
+            // value on the stack, then call constructor ctor1.
+            ctor0IL.Emit(OpCodes.Ldarg_0);
+            ctor0IL.Emit(OpCodes.Ldc_I4_S, 42);
+            ctor0IL.Emit(OpCodes.Call, ctor1);
+            ctor0IL.Emit(OpCodes.Ret);
+
+
+            PropertyBuilder pbNumber = tb.DefineProperty("Number", PropertyAttributes.HasDefault, typeof(int), null);
+            MethodAttributes getSetAttr = MethodAttributes.Public |
+            MethodAttributes.SpecialName | MethodAttributes.HideBySig;
+
+            MethodBuilder mbNumberGetAccessor = tb.DefineMethod("get_Number", getSetAttr, typeof(int), Type.EmptyTypes);
+
+            ILGenerator numberGetIL = mbNumberGetAccessor.GetILGenerator();
+
+            numberGetIL.Emit(OpCodes.Ldarg_0);
+            numberGetIL.Emit(OpCodes.Ldfld, fbNumber);
+            numberGetIL.Emit(OpCodes.Ret);
+
+            MethodBuilder mbNumberSetAccessor = tb.DefineMethod(
+        "set_Number",
+        getSetAttr,
+        null,
+        new Type[] { typeof(int) });
+
+            ILGenerator numberSetIL = mbNumberSetAccessor.GetILGenerator();
+            // Load the instance and then the numeric argument, then store the
+            // argument in the field.
+            numberSetIL.Emit(OpCodes.Ldarg_0);
+            numberSetIL.Emit(OpCodes.Ldarg_1);
+            numberSetIL.Emit(OpCodes.Stfld, fbNumber);
+            numberSetIL.Emit(OpCodes.Ret);
+
+
+            pbNumber.SetGetMethod(mbNumberGetAccessor);
+            pbNumber.SetSetMethod(mbNumberSetAccessor);
+
+            MethodBuilder meth = tb.DefineMethod(
+       "MyMethod",
+       MethodAttributes.Public,
+       typeof(int),
+       new Type[] { typeof(int) });
+
+            ILGenerator methIL = meth.GetILGenerator();
+
+            methIL.Emit(OpCodes.Ldarg_0);
+            methIL.Emit(OpCodes.Ldfld, fbNumber);
+            methIL.Emit(OpCodes.Ldarg_1);
+            methIL.Emit(OpCodes.Mul);
+            methIL.Emit(OpCodes.Ret);
+
+            Type t = tb.CreateType();
+
+            //    ab.Save(aName.Name + ".dll");
+            MethodInfo mi = t.GetMethod("MyMethod");
+            PropertyInfo pi = t.GetProperty("Number");
+            object o1 = Activator.CreateInstance(t);
+            Console.WriteLine("o1.Number: {0}", pi.GetValue(o1, null));
+            pi.SetValue(o1, 127, null);
+            Console.WriteLine("o1.Number: {0}", pi.GetValue(o1, null));
+            object[] arguments = { 22 };
+            Console.WriteLine("o1.MyMethod(22): {0}",
+                mi.Invoke(o1, arguments));
+
+            object o2 = Activator.CreateInstance(t,
+     new object[] { 5280 });
+            Console.WriteLine("o2.Number: {0}", pi.GetValue(o2, null));
+
+            Console.ReadKey();
+        }
+
+    }
+}
